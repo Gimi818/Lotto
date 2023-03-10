be416d645906d5c2cada5869ff81e32397d4eb57
Gimi818 <gimi8183@gmail.com>
2023-03-10 15:14:30 +0100

    first commit

---
+++ src/test/java/com/lotto/domain/numbergenerator/NumberGeneratorFacadeTest.java
@@ -0,0 +1,12 @@
+package com.lotto.domain.numbergenerator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGeneratorFacadeTest {
+    @Test
+    public  void test(){
+
+    }
+}
